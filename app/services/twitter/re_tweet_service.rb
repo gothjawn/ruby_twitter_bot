5cad182336575b7887b70fcc5d2bd8318cebee35
Lexi C <cummins.a88@gmail.com>
2021-05-04 17:55:26 -0400

    adding gitignore and api keys

--- app/services/twitter/re_tweet_service.rb
+++ app/services/twitter/re_tweet_service.rb
@@ -1,3 +1,11 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'twitter'
+require 'figaro'
+
+Figaro.application = Figaro::Application.new(
+    environment: 'production',
+    path: File.expand_path('config/application.yml')
+)
+
+Figaro.load
