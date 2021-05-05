263e40f449de60901bcff0f2d3026b94f15d0956
Lexi C <cummins.a88@gmail.com>
2021-05-05 10:04:31 -0400

    twitter module

--- app/services/twitter/re_tweet_service.rb
+++ app/services/twitter/re_tweet_service.rb
@@ -9,3 +9,21 @@ Figaro.application = Figaro::Application.new(
 )
 
 Figaro.load
+
+
+module Twitter
+    class ReTweetService
+        attr_reader :config
+
+        def initialize 
+            @config = twitter_api_config
+        end
+
+        def perform 
+            rest_client = configure_rest_client
+            stream_client = configure_stream_client
+        end
+
+        private
+    end
+end
