6e783c0077709b0062241bc48d3c477ec46a42fb
Lexi C <cummins.a88@gmail.com>
2021-05-06 13:38:22 -0400

    added string

--- app/services/twitter/re_tweet_service.rb
+++ app/services/twitter/re_tweet_service.rb
@@ -22,9 +22,14 @@ module Twitter
         def perform 
             rest_client = configure_rest_client
             stream_client = configure_stream_client
+
+            while true 
+                puts 'Starting to Retweet 3, 2, 1...NOW!'
+            end
         end
 
         private
+        HASHTAGS_TO_WATCH = %W[#rails #ruby #RubyOnRails]
 
         def twitter_api_config
         {
@@ -49,5 +54,3 @@ module Twitter
     end
 end
 
-private
-HASHTAGS_TO_WATCH = %W[#rails #ruby #RubyOnRails]
